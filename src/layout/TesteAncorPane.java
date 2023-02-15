193788f5a42045e76edc860cdb16ff14eb345b83
Nesrux <joaomarcosdevs@gmail.com>
2023-02-15 16:05:34 -0300

    Centro centralizado

--- src/layout/TesteAncorPane.java
+++ src/layout/TesteAncorPane.java
@@ -1,6 +1,8 @@
 package layout;
 
+import javafx.geometry.Pos;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 
 public class TesteAncorPane extends AnchorPane {
 
@@ -20,8 +22,19 @@ public class TesteAncorPane extends AnchorPane {
 		Quadrado q4 = new Quadrado();
 		setBottomAnchor(q4, 10.0);
 		setRightAnchor(q4, 10.0);
-
-		getChildren().addAll(q1, q2, q3, q4);
+		
+		Quadrado q5 = new Quadrado();
+		
+		HBox centro = new HBox();
+		centro.setAlignment(Pos.CENTER);
+		setTopAnchor(centro, 110.0);
+		setBottomAnchor(centro, 110.0);
+		setLeftAnchor(centro, 110.0);
+		setRightAnchor(centro, 110.0);
+		
+		centro.getChildren().add(q5);
+		
+		getChildren().addAll(q1, q2, q3, q4, centro);
 
 	}
 
